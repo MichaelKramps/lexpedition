71e49ea26d05fe23dece9decee509b25ea5d158f
Michael Kramps <michael@kinship.games>
2023-05-03 11:24:17 -0500

    add tutorial paths prompt

--- lib/src/main_menu/new_player_menu.dart
+++ lib/src/main_menu/new_player_menu.dart
@@ -2,28 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lexpedition/src/game_data/constants.dart';
 
-class NewPlayerMenu extends StatelessWidget {
+class NewPlayerMenu extends StatefulWidget {
   const NewPlayerMenu({super.key});
 
   @override
+  State<NewPlayerMenu> createState() => _NewPlayerMenuState();
+}
+
+class _NewPlayerMenuState extends State<NewPlayerMenu> {
+  bool _showTutorialBox = false;
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              GoRouter.of(context).push('/tutorial');
-            },
-            child: const Text('Tutorial'),
+    return Stack(
+      children: [
+        SizedBox.expand(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _showTutorialBox = true;
+                  });
+                },
+                child: const Text('Tutorial'),
+              ),
+              SizedBox(width: Constants.smallFont),
+              ElevatedButton(
+                onPressed: () => GoRouter.of(context).push('/settings'),
+                child: const Text('Settings'),
+              )
+            ],
           ),
-          SizedBox(width: Constants.smallFont),
-          ElevatedButton(
-            onPressed: () => GoRouter.of(context).push('/settings'),
-            child: const Text('Settings'),
-          ),
-        ],
-      ),
+        ),
+        Visibility(
+          visible: _showTutorialBox,
+          child: AlertDialog(
+              content: Text('Which Tutorial path will you take?'),
+              actions: [
+                ElevatedButton(
+                  child: const Text('Full Tutorial'),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  child: const Text('Quick Tutorial'),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  child: const Text('Skip Tutorial'),
+                  onPressed: () {},
+                ),
+                ElevatedButton(
+                  child: const Text('Cancel'),
+                  onPressed: () {
+                    setState(() {
+                      _showTutorialBox = false;
+                    });
+                  },
+                ),
+              ]),
+        )
+      ],
     );
   }
 }
