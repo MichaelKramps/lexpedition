1bfe0158d57e9a6e3a9ed2da155c1f61513f0986
Michael Kramps <michael@kinship.games>
2023-05-25 14:01:48 -0500

    show partners screen in right column

--- lib/src/game_widgets/two_player_right_column_widget.dart
+++ lib/src/game_widgets/two_player_right_column_widget.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:lexpedition/src/game_data/game_state.dart';
+import 'package:lexpedition/src/game_widgets/letter_grid_widget.dart';
+import 'package:provider/provider.dart';
 
 class TwoPlayerRightColumnWidget extends StatelessWidget {
   final GameState gameState;
 
-  const TwoPlayerRightColumnWidget(
-      {super.key, required this.gameState});
+  const TwoPlayerRightColumnWidget({super.key, required this.gameState});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: () => gameState.toggleVisibleScreen(), child: Text('Switch'));
+    return Consumer<GameState>(builder: (context, gameState, child) {
+      if (gameState.getTheirGrid() != null && gameState.getMyGrid() != null) {
+        return FittedBox(
+          child: LetterGridWidget(
+            gameState: gameState, 
+            letterGrid: gameState.getTheirGrid()!
+          )
+        );
+      } else {
+        return SizedBox.shrink();
+      }
+    });
   }
 }
