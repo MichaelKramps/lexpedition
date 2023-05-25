e19f483921683303e69cbb977668e15f8e57592c
Michael Kramps <michael@kinship.games>
2023-05-25 14:09:52 -0500

    add some buttons to the two player right column

--- lib/src/game_widgets/two_player_right_column_widget.dart
+++ lib/src/game_widgets/two_player_right_column_widget.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lexpedition/src/game_data/game_mode.dart';
 import 'package:lexpedition/src/game_data/game_state.dart';
 import 'package:lexpedition/src/game_widgets/letter_grid_widget.dart';
+import 'package:lexpedition/src/level_info/level_db_connection.dart';
+import 'package:lexpedition/src/user_interface/basic_game_button.dart';
 import 'package:provider/provider.dart';
 
 class TwoPlayerRightColumnWidget extends StatelessWidget {
@@ -10,17 +14,56 @@ class TwoPlayerRightColumnWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<GameState>(builder: (context, gameState, child) {
-      if (gameState.getTheirGrid() != null && gameState.getMyGrid() != null) {
-        return FittedBox(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BasicGameButton(
+          onPressed: () {
+            gameState.resetPuzzle(notify: true);
+            logPuzzleQuit();
+          },
+          buttonText: 'Restart',
+        ),
+        SizedBox(height: 12),
+        determinePartnerScreen(),
+        SizedBox(height: 12),
+        BasicGameButton(
+        onPressed: () {
+          logPuzzleQuit();
+          GoRouter.of(context).push('/');
+        },
+        buttonText: 'Home',
+      )
+      ],
+    );
+  }
+
+  Widget determinePartnerScreen() {
+    if (gameState.getTheirGrid() != null && gameState.getMyGrid() != null) {
+      return FittedBox(
           child: LetterGridWidget(
-            gameState: gameState, 
-            letterGrid: gameState.getTheirGrid()!
-          )
-        );
-      } else {
-        return SizedBox.shrink();
+              gameState: gameState, letterGrid: gameState.getTheirGrid()!));
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+
+  void logPuzzleQuit() {
+    if (gameState.level.puzzleId != null) {
+      switch (gameState.gameMode) {
+        case GameMode.OnePlayerFreePlay:
+        case GameMode.OnePlayerLexpedition:
+          LevelDatabaseConnection.logOnePlayerUnfinishedPuzzleResults(
+            gameState.level.puzzleId as int);
+          break;
+        case GameMode.TwoPlayerFreePlay:
+        case GameMode.TwoPlayerLexpedition:
+          LevelDatabaseConnection.logTwoPlayerUnfinishedPuzzleResults(
+            gameState.level.puzzleId as int);
+          break;
+        default:
+          break;
       }
-    });
+    }
   }
 }
