2bfe4df776d078616e93221dc2633c178709cf21
Michael Kramps <michael@kinship.games>
2023-06-07 10:15:24 -0500

    get two player lexpedition builder working

--- lib/src/build_puzzle/build_two_player_lexpedition_widget.dart
+++ lib/src/build_puzzle/build_two_player_lexpedition_widget.dart
@@ -18,18 +18,11 @@ class BuildTwoPlayerLexpeditionWidget extends StatefulWidget {
 
 class _BuildTwoPlayerLexpeditionWidgetState
     extends State<BuildTwoPlayerLexpeditionWidget> {
-  LetterGrid _grid = new LetterGrid(blankGrid);
-  LetterGrid _gridB = new LetterGrid(blankGrid);
+  LetterGrid _grid = new LetterGrid.forLexpeditionBuilder();
+  LetterGrid _gridB = new LetterGrid.forLexpeditionBuilder();
   bool _selectedGrid = true;
   int _selectedIndex = -1;
   int _step = 0;
-
-  GlobalKey gridKey = GlobalKey();
-  late RenderBox renderBox =
-      gridKey.currentContext?.findRenderObject() as RenderBox;
-  late Offset gridPosition = renderBox.localToGlobal(Offset.zero);
-  late double _gridx = gridPosition.dx;
-  late double _gridy = gridPosition.dy;
 
   @override
   Widget build(BuildContext context) {
@@ -59,21 +52,23 @@ class _BuildTwoPlayerLexpeditionWidgetState
                     onPressed: () => {
                           setState(() => {_step = 3})
                         },
-                    child: Text('.')),
+                    child: Text('(|)')),
                 ElevatedButton(
                     onPressed: () => {
-                          setState(() => {_step = 4})
+                          setState(() {
+                            _grid.addColumn();
+                            _gridB.addColumn();
+                          })
                         },
-                    child: Text('(|)')),
+                    child: Text('+|')),
               ]),
-              SizedBox(height: 20),
               Stack(children: [
                 Visibility(
                     visible: showOptionOnStep(1),
                     child: Column(children: [
-                      buildTileTypeButton('blue-basic', TileType.basic),
-                      buildTileTypeButton('green-start', TileType.start),
-                      buildTileTypeButton('red-end', TileType.end),
+                      buildTileTypeButton(TileType.basic),
+                      buildTileTypeButton(TileType.start),
+                      buildTileTypeButton(TileType.end),
                     ])),
                 Visibility(
                     visible: showOptionOnStep(2),
@@ -122,17 +117,7 @@ class _BuildTwoPlayerLexpeditionWidgetState
                       ],
                     )),
                 Visibility(
-                    visible: showOptionOnStep(3),
-                    child: Column(
-                      children: [
-                        buildChargesButton(1),
-                        buildChargesButton(2),
-                        buildChargesButton(3),
-                        buildChargesButton(4)
-                      ],
-                    )),
-                Visibility(
-                  visible: showOptionOnStep(4),
+                  visible: showOptionOnStep(3),
                   child: Column(children: [
                     buildObstacleButton(true),
                     buildObstacleButton(false),
@@ -149,21 +134,30 @@ class _BuildTwoPlayerLexpeditionWidgetState
                 });
               },
               child: Text('Switch Puzzles')),
-          Listener(
-              key: gridKey,
-              onPointerDown: selectTile,
-              child: Row(children: [
-                for (var column in getSelectedGrid().columns) ...[
-                  Column(children: [
-                    for (var letterTile in column) ...[
-                      LetterTileWidget(
-                          letterTile: letterTile,
-                          gameState: new GameState.emptyState(),
-                          blastDirection: getSelectedGrid().blastDirection)
+          SizedBox(
+              height: Constants().gridHeight(),
+              width: Constants().gridWidth() + Constants().tileMargin(),
+              child: ListView(
+                  padding: EdgeInsets.all(0),
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    for (var column in getSelectedGrid().columns) ...[
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            for (LetterTile letterTile in column) ...[
+                              Listener(
+                                  onPointerDown: (event) {
+                                    selectTile(letterTile.index);
+                                  },
+                                  child: LetterTileWidget(
+                                      letterTile: letterTile,
+                                      gameState: new GameState.emptyState(),
+                                      blastDirection: _grid.blastDirection))
+                            ]
+                          ])
                     ]
-                  ])
-                ]
-              ]))
+                  ])),
         ]),
         Column(children: [
           SizedBox(height: 30),
@@ -205,81 +199,24 @@ class _BuildTwoPlayerLexpeditionWidgetState
     ]);
   }
 
-  void selectTile(event) {
-    double pointerx = event.position.dx;
-    double pointery = event.position.dy;
-
-    int xDistance = (pointerx - _gridx).round();
-    int yDistance = (pointery - _gridy).round();
-
-    int row = -1;
-    int column = -1;
-
-    Constants constants = Constants();
-    if (yDistance > constants.tileOneStart() &&
-        yDistance < constants.tileOneEnd()) {
-      row = 0;
-    } else if (yDistance > constants.tileTwoStart() &&
-        yDistance < constants.tileTwoEnd()) {
-      row = 1;
-    } else if (yDistance > constants.tileThreeStart() &&
-        yDistance < constants.tileThreeEnd()) {
-      row = 2;
-    } else if (yDistance > constants.tileFourStart() &&
-        yDistance < constants.tileFourEnd()) {
-      row = 3;
-    }
-
-    if (xDistance > constants.tileOneStart() &&
-        xDistance < constants.tileOneEnd()) {
-      column = 0;
-    } else if (xDistance > constants.tileTwoStart() &&
-        xDistance < constants.tileTwoEnd()) {
-      column = 1;
-    } else if (xDistance > constants.tileThreeStart() &&
-        xDistance < constants.tileThreeEnd()) {
-      column = 2;
-    } else if (xDistance > constants.tileFourStart() &&
-        xDistance < constants.tileFourEnd()) {
-      column = 3;
-    } else if (xDistance > constants.tileFiveStart() &&
-        xDistance < constants.tileFiveEnd()) {
-      column = 4;
-    } else if (xDistance > constants.tileSixStart() &&
-        xDistance < constants.tileSixEnd()) {
-      column = 5;
-    }
-
-    int selectedIndex;
-
-    if (row < 0 || column < 0) {
-      selectedIndex = -1;
-    } else {
-      selectedIndex = (column * 4) + (row);
-    }
-
-    if (selectedIndex == _selectedIndex) {
-      // something else
-    } else {
-      setState(() {
-        if (_selectedIndex > -1) {
-          getSelectedGrid().letterTiles[_selectedIndex].unselect();
-        }
-        _selectedIndex = selectedIndex;
-        getSelectedGrid().letterTiles[selectedIndex].select();
-      });
-    }
+  void selectTile(int index) {
+    LetterGrid thisGrid = _selectedGrid ? _grid : _gridB;
+    setState(() {
+      _grid.unselectAll();
+      _gridB.unselectAll();
+      _selectedIndex = index;
+      thisGrid.letterTiles[index].select();
+    });
   }
 
   bool showOptionOnStep(int step) {
     return step == _step;
   }
 
-  Widget buildTileTypeButton(String path, TileType type) {
+  Widget buildTileTypeButton(TileType type) {
     return ElevatedButton(
         onPressed: () => {setLetterTile(tileType: type)},
-        child: Image.asset('assets/images/' + path + '.png',
-            width: Constants().tileSize(), height: Constants().tileSize()));
+        child: Text(type.toString()));
   }
 
   Widget buildLetterButton(String letter) {
@@ -288,15 +225,6 @@ class _BuildTwoPlayerLexpeditionWidgetState
             TextButton.styleFrom(fixedSize: Size.square(Constants.smallFont)),
         onPressed: () => {setLetterTile(letter: letter)},
         child: Text(letter.toUpperCase(),
-            style: TextStyle(fontSize: Constants.smallFont)));
-  }
-
-  Widget buildChargesButton(int numberCharges) {
-    return ElevatedButton(
-        style:
-            TextButton.styleFrom(fixedSize: Size.square(Constants.smallFont)),
-        onPressed: () => {setLetterTile(requiredCharges: numberCharges)},
-        child: Text(numberCharges.toString(),
             style: TextStyle(fontSize: Constants.smallFont)));
   }
 
@@ -315,10 +243,7 @@ class _BuildTwoPlayerLexpeditionWidgetState
   }
 
   void setLetterTile(
-      {TileType? tileType,
-      String? letter,
-      int? requiredCharges,
-      int? requiredObstacleCharges}) {
+      {TileType? tileType, String? letter, int? requiredObstacleCharges}) {
     setState(() {
       LetterTile letterTile = getSelectedGrid().letterTiles[_selectedIndex];
 
@@ -326,16 +251,13 @@ class _BuildTwoPlayerLexpeditionWidgetState
           ? TileType.basic
           : letterTile.tileType;
       String backupLetter = letterTile.letter == '' ? 'a' : letterTile.letter;
-      int backupCharges =
-          letterTile.requiredCharges == 0 ? 2 : letterTile.requiredCharges;
       int backupObstacleCharges = letterTile.requiredObstacleCharges == 0
           ? 0
           : letterTile.requiredObstacleCharges;
 
       letterTile.tileType = tileType == null ? backupType : tileType;
       letterTile.letter = letter == null ? backupLetter : letter;
-      letterTile.requiredCharges =
-          requiredCharges == null ? backupCharges : requiredCharges;
+      letterTile.requiredCharges = 1;
       letterTile.requiredObstacleCharges = requiredObstacleCharges == null
           ? backupObstacleCharges
           : requiredObstacleCharges;
